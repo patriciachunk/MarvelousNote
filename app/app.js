6d08ae6bf3aa2c6643a4c767e6f9b0f7339dd66f
Patricia Chun <yejeechun@gmail.com>
2016-06-19 19:10:58 -0400

    Add NotesController.

--- app/app.js
+++ app/app.js
@@ -1,22 +1,13 @@
 (function() {
   var app = angular.module('marvelousnote', [
-    'ui.router'
+    'ui.router',
+    'marvelousnote.notes'
   ]);
 
-  function config($stateProvider, $urlRouterProvider) {
+  function config($urlRouterProvider) {
     $urlRouterProvider.otherwise('/notes');
-
-    $stateProvider
-
-      .state('notes', {
-        url: '/notes',
-        template: '<h1>MarvelousNote</h1><p>{{ message }}</p>',
-        controller: function($scope) {
-          $scope.message = "Welcome to MarvelousNote!";
-        }
-      });
   }
 
-  config['$inject'] = ['$stateProvider', '$urlRouterProvider'];
+  config.$inject = ['$urlRouterProvider'];
   app.config(config);
 })();
