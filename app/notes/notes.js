35687c494b164f4e7d93e2fac3ae0d5058b01d9a
Patricia Chun <yejeechun@gmail.com>
2016-06-19 19:29:10 -0400

    Add note form.

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -9,9 +9,14 @@ angular.module('marvelousnote.notes', [
       url: '/notes',
       templateUrl: 'notes/notes.html',
       controller: 'NotesController'
+    })
+
+
+    .state('notes.form', {
+      url: '/:noteId',
+      templateUrl: 'notes/notes-form.html'
     });
 })
 
-.controller('NotesController', function($scope){
-  $scope.message = 'I <3 angular';
+.controller('NotesController', function(){
 });
