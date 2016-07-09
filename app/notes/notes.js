2a5a11388612792f35bfe409aca21bb1b0afb9c4
Patricia Chun <yejeechun@gmail.com>
2016-07-09 17:26:58 -0400

    Don't clear the form after saving.

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -40,9 +40,11 @@
         NotesService.update($scope.note);
       }
       else {
-        NotesService.create($scope.note);
+        NotesService.create($scope.note)
+          .then(function(res) {
+            $scope.note = res.data.note;
+          });
       }
-      $scope.clearForm();
     };
 
     $scope.edit = function(note) {
