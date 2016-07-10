a85981f5c18e1c61ac8d86e3668ea0e4739b1726
Patricia Chun <yejeechun@gmail.com>
2016-07-09 22:28:36 -0400

    Update app module to confirm to style guide.

--- app/app.module.js
+++ app/app.module.js
@@ -1,15 +1,14 @@
 (function() {
-  var app = angular.module('marvelousnote', [
+  angular.module('marvelousnote', [
     'ui.router',
     'ngFlash',
     'textAngular',
     'marvelousnote.notes'
-  ]);
+  ])
+    .config(config);
 
+  config.$inject = ['$urlRouterProvider'];
   function config($urlRouterProvider) {
     $urlRouterProvider.otherwise('/notes/');
   }
-
-  config.$inject = ['$urlRouterProvider'];
-  app.config(config);
 })();
